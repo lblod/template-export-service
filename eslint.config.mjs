690a3a183b4a47e6ce49a85adc72944349a443ec
Elena Poelman <elena.poelman@redpencil.io>
2024-12-17 18:57:20 +0100

    linting: set-up typescript-eslint

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,10 +1,12 @@
 import globals from 'globals';
 import pluginJs from '@eslint/js';
+import pluginTs from 'typescript-eslint';
 import eslintPluginPrettierRecommended from 'eslint-plugin-prettier/recommended';
 
 /** @type {import('eslint').Linter.Config[]} */
 export default [
   pluginJs.configs.recommended,
+  ...pluginTs.configs.recommended,
   eslintPluginPrettierRecommended,
   {
     languageOptions: {
@@ -15,6 +17,10 @@ export default [
     rules: {
       semi: ['error', 'always'],
       'linebreak-style': ['error', 'unix'],
+      '@typescript-eslint/no-unused-vars': [
+        'error',
+        { varsIgnorePattern: '^_', argsIgnorePattern: '^_' },
+      ],
     },
   },
 ];
