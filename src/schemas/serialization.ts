5f1af89458a0b6bb0717d92153dc1e6449dd35d0
Elena Poelman <elena.poelman@redpencil.io>
2025-01-06 14:34:37 +0100

    schemas: add serialization zod schema

---
+++ src/schemas/serialization.ts
@@ -0,0 +1,18 @@
+import { z } from 'zod';
+import { DocumentContainerSchema } from './document-container';
+import { EditorDocumentSchema } from './editor-document';
+import { SnippetListSchema } from './snippet-list';
+import { SnippetSchema } from './snippet';
+import { SnippetVersionSchema } from './snippet-version';
+
+export const SerializationSchema = z.object({
+  documentContainers: z.array(DocumentContainerSchema),
+  editorDocuments: z.array(EditorDocumentSchema),
+  snippetLists: z.array(SnippetListSchema),
+  snippets: z.array(SnippetSchema),
+  snippetVersions: z.array(SnippetVersionSchema),
+});
+
+export type Serialization = z.infer<typeof SerializationSchema>;
+export type Export = Serialization;
+export type Import = Serialization;
