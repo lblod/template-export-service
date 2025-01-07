efb5e53d25f6c5b127fa60554c2927d802f1416b
Elena Poelman <elena.poelman@redpencil.io>
2025-01-07 10:59:59 +0100

    add db function to create archive resources

---
+++ src/db/archive.ts
@@ -0,0 +1,23 @@
+import { sparqlEscapeString, sparqlEscapeUri, update, uuid } from 'mu';
+import { Archive } from '../schemas/archive';
+import { Optional } from '../utils/types';
+
+export async function createArchive(data: Optional<Archive, 'id' | 'uri'>) {
+  const id: string = data.id ?? uuid();
+  const uri: string = data.uri ?? `http://redpencil.data.gift/id/archive/${id}`;
+  await update(/* sparql */ `
+        PREFIX mu: <http://mu.semte.ch/vocabularies/core/>
+        PREFIX nfo: <http://www.semanticdesktop.org/ontologies/2007/03/22/nfo#>
+        PREFIX task: <http://redpencil.data.gift/vocabularies/tasks/>
+        INSERT DATA {
+          ${sparqlEscapeUri(uri)} 
+            a nfo:Archive;
+              mu:uuid ${sparqlEscapeString(id)};
+              task:hasFile ${sparqlEscapeUri(data.fileUri)}.
+        }`);
+  return {
+    ...data,
+    uri,
+    id,
+  };
+}
